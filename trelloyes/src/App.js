af86a002a4586015c3ef4da5b75e977992a795a4
Nathan Leung <nath.leung@gmail.com>
2019-10-07 12:46:22 -0700

    Trelloyes complete

--- trelloyes/src/App.js
+++ trelloyes/src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import List from './List'
+// import store from './store';
+import './App.css'
 
-function App() {
+function App(props) {
+
+  
   return (
-    <div className="App">
+    <main className='App'>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Trelloyes!</h1>
       </header>
-    </div>
+      <div className="App-list">
+    {props.store.lists.map(list=> {
+      return (
+          <List header={list.header} cards={list.cardIds.map(card =>{
+            return(
+              props.store.allCards[card]
+            )
+          })
+          } key={list.id}/>   
+        );
+      })
+    }      
+      </div>
+    </main>
   );
 }
 
