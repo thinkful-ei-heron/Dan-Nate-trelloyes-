3562f82f9af3ccef26a6ce643dc2c11e6f628a9f
Nathan Leung <nath.leung@gmail.com>
2019-10-08 08:26:23 -0700

    first commit

---
+++ Julio-Nate-trelloyes-testing/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import List from './List'
+// import store from './store';
+import './App.css'
+
+function App(props) {
+
+  
+  return (
+    <main className='App'>
+      <header className="App-header">
+        <h1>Trelloyes!</h1>
+      </header>
+      <div className="App-list">
+    {props.store.lists.map(list=> {
+      return (
+          <List header={list.header} cards={list.cardIds.map(card =>{
+            return(
+              props.store.allCards[card]
+            )
+          })
+          } key={list.id}/>   
+        );
+      })
+    }      
+      </div>
+    </main>
+  );
+}
+
+export default App;
